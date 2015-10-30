b92f755672b671c52058cea9730da1292370a513
mtkubota <mattkay5@gmail.com>
2015-10-29 21:17:53 -1000

    meal choice

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,2 +1,10 @@
-# Define a method meal_choice that returns the meal_choice that was
+def meal_choice(name = "meat")
+  puts name 
+  name
+
+end
+
+meal_choice
+meal_choice("vegan")
+ # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
