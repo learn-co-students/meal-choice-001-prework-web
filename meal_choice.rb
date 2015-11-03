2f11c93bed2ef826826b23d7aa7723575685acc4
maryjanemedina <maryjane.medina.simontech2016@gmail.com>
2015-11-03 22:38:13 +0000

    the lab is done

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,2 +1,21 @@
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
+
+def meal_choice(choice = "meat")
+  return choice #meal choice that was passed into it 
+end
+
+puts meal_choice("motha effin tannies")
+puts meal_choice
+puts meal_choice("chicken")
+puts meal_choice
+
+# def directions 
+#   puts "astor is the homie"
+#   puts "got right"
+#   puts "go straight 2 miles"
+#   put " go into drive way"
+# end 
+  
+
+  
