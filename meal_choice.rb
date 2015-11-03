e68b938def045a8464b4d374977701e850398d2e
AnahisD <duenasanahis@gmail.com>
2015-11-03 22:55:00 +0000

    meal choice mission complete

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,2 +1,17 @@
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
+
+def meal_choice (meat = "meat")
+  return choice #meal choice that was passed into it
+end
+puts meal_choice("cat food")
+puts meal_choice
+puts meal_choice("dog food")
+puts meal_choice
+
+#def directions
+#def  puts  "astor is the homie"
+#def   puts "go right"
+#def   puts "go straight 2 miles"
+#def   puts "go into drive way
+# end
