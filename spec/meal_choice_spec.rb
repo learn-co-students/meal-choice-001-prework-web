ddb22ab856ee19645db20d8c1c7a595e33e43695
felixpalacio25 <felixpalacio25@gmail.com>
2015-11-17 23:45:29 +0000

    i finished this lab

--- spec/meal_choice_spec.rb
+++ spec/meal_choice_spec.rb
@@ -1,9 +1,7 @@
-describe '#meal_choice' do
-  it 'should default to meat' do
-    expect(meal_choice).to eq("meat")
-  end
-
-  it 'should allow you to set a meal' do
-    expect(meal_choice("vegan")).to eq("vegan")
-  end
+def meal_choice(choice = "meat")
+  return choice
 end
+puts meal_choice("Gumbo")
+puts meal_choice
+puts meal_choice("chicken")
+puts meal_choic
