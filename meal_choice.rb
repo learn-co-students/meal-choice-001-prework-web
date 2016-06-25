fde3657e2be2c9b21e9ab740b55c8b4277847b56
Ekaterina Petushova <jevsador@yandex.ru>
2016-06-25 16:07:02 +0000

    Done.

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,9 +1,8 @@
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
-def meal_choice(name = "meat")
-    puts "The Meal Choice is #{name}!"
+def meal_choice(meal="meat")
+  meal_choice=meal
 end
 
-def meal_choice(name = "meat")
-    meal_choice = "#{name}"
-end
+meal_choice
+meal_choice("vegan")
